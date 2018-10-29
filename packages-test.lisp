f4d747185b48a017691b418e4689ed385c45f73a
zVcXr <zvcxr666@gmail.com>
2018-10-29 19:20:39 +0200

    defpackage & auto indent

--- packages-test.lisp
+++ packages-test.lisp
@@ -1,30 +1,26 @@
 (defpackage :test
-  (:use :common-lisp :alexandria)
-  (:export
-   :check
-   :deftest))
+  (:use #:common-lisp
+	#:alexandria)
+  (:export #:check
+	   #:deftest))
 
 (defpackage :html-gen
-  (:use
-   :common-lisp
-   :alexandria
-   :test)
-  (:export
-   :html
-   :xml
-   :xml-declaration
-   :generate-html
-   :generate-xml))
+  (:use #:common-lisp
+	#:alexandria
+	#:test)
+  (:export #:html
+	   #:xml
+	   #:xml-declaration
+	   #:generate-html
+	   #:generate-xml))
 
 (defpackage :epub
-  (:use
-   :common-lisp
-   :alexandria
-   :html-gen
-   :test)
-  (:export
-   :write-mimetype
-   :write-container-xml
-   :write-content
-   :write-package-document
-   :create-metadata))
+  (:use #:common-lisp
+	#:alexandria
+	#:html-gen
+	#:test)
+  (:export #:write-mimetype
+	   #:write-container-xml
+	   #:write-content
+	   #:write-package-document
+	   #:create-metadata))
--- packages.lisp
+++ packages.lisp
@@ -1,22 +1,18 @@
 (defpackage :html-gen
-  (:use
-   :common-lisp
-   :alexandria)
-  (:export
-   :html
-   :xml
-   :xml-declaration
-   :generate-html
-   :generate-xml))
+  (:use #:common-lisp
+	#:alexandria)
+  (:export #:html
+	   #:xml
+	   #:xml-declaration
+	   #:generate-html
+	   #:generate-xml))
 
 (defpackage :epub
-  (:use
-   :common-lisp
-   :html-gen
-   :alexandria)
-  (:export
-   :write-mimetype
-   :write-container-xml
-   :write-content
-   :write-package-document
-   :create-metadata))
+  (:use #:common-lisp
+	#:html-gen
+	#:alexandria)
+  (:export #:write-mimetype
+	   #:write-container-xml
+	   #:write-content
+	   #:write-package-document
+	   #:create-metadata))
